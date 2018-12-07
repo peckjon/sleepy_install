4196a374f8ce3fab907c2d966adf92236ad8782f
Jonathan Peck <peckjon@gmail.com>
2018-12-07 10:09:05 -0800

    Minor version indicates wait time

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+rm dist/*
 python3 setup.py sdist bdist_wheel
-#only upload the tar.hz, not the .whl
-twine upload dist/sleepy_install-0.0.2.tar.gz
+#only upload the tar.hz, not the .whl, or the installation delay gets eliminated
+twine upload dist/*.gz
--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
 import time
 import setuptools
 
-t = 60*5
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+t = 3
 
 setuptools.setup(
     name="sleepy_install",
-    version="0.0.2",
+    version="0.%d.1" % t,
     author="Jon Peck",
     author_email="peckjon@gmail.com",
-    description="Waits for %d seconds during package install. No other features; use for testing time-dependent automation." % t,
-    long_description=long_description,
+    description="Waits for [minor version number] minutes during package install. No other features; use for testing time-dependent automation.",
+    long_description="Wait for several minutes during package installation. No other features; use for testing time-dependent automation. Minor version indicates wait time (e.g. for a 2 minute delay, use 0.2.*)",
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
     packages=[],
@@ -23,6 +20,6 @@ setuptools.setup(
     ],
 )
 
-print("Begin %d second wait..." % t)
-time.sleep(t)
+print("Begin %d minute wait..." % t)
+time.sleep(t*60)
 print("End wait.")
