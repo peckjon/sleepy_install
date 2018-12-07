f20a97b271f4888080f76ac22cb908f9c87792e4
Jon Peck <peckjon@gmail.com>
2018-12-06 17:13:28 -0800

    nope

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
-    packages=["time"],
+    packages=[],
     scripts=["wait.py"],
     classifiers=[
         "Programming Language :: Python :: 3",
