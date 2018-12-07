f46c2cb55c2ed8c355e69326942d997c49089337
Jon Peck <peckjon@gmail.com>
2018-12-06 17:05:02 -0800

    Default 60-second wait

--- setup.py
+++ setup.py
@@ -3,18 +3,17 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-time.sleep(60)
-
 setuptools.setup(
     name="sleepy_install",
     version="0.0.1",
     author="Jon Peck",
     author_email="peckjon@gmail.com",
-    description="Waits for N minutes during package install. No other features; use for testing time-dependent automation.",
+    description="Waits for 60 seconds during package install. No other features; use for testing time-dependent automation.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
     packages=[],
+    scripts=[wait.py],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: WTFPL",
